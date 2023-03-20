86380a486f2477ed4c129a897f34a9c4234c90c7
Petr Oblouk <ahojcz48@gmail.com>
2023-03-20 08:54:47 +0100

    add files

--- grafy_app.py
+++ grafy_app.py
@@ -2,6 +2,8 @@
 
 from os.path import basename, splitext
 import tkinter as tk
+from tkinter import filedialog # prohlížeč souborů
+import pylab as lab # grafy
 
 # from tkinter import ttk
 
@@ -39,10 +41,10 @@ class Application(tk.Tk):
 
     #* Výběr souboru - FRAME
         self.fileFrame = tk.LabelFrame(self, text='Soubor')
-        self.fileFrame.pack(padx=5, pady=5)
+        self.fileFrame.pack(padx=5, pady=5, fill='x') # fill je roztažení ve směru osy x
         self.fileEntry = MyEntry(self.fileFrame)
-        self.fileEntry.pack()
-        self.fileBtn = tk.Button(self.fileFrame, text='...')
+        self.fileEntry.pack(fill='x')
+        self.fileBtn = tk.Button(self.fileFrame, text='...', command=self.fileSelect)
         self.fileBtn.pack(anchor='e')
         
         #! Výběr formátu grafu
@@ -52,9 +54,11 @@ class Application(tk.Tk):
         self.sloupceRbtn = tk.Radiobutton(self.fileFrame, text="Data jsou ve sloupcích", variable=self.dataformatVar, value=1)
         self.sloupceRbtn.pack(anchor='w')
 
+        #! Tlačítko kreslit
+        tk.Button(self, text='Kreslit', command=self.plotGraph).pack(anchor='w')
+
         #! Tlačítko quit
-        self.btn = tk.Button(self, text="Quit", command=self.quit)
-        self.btn.pack
+        tk.Button(self, text='Quit', command=self.quit).pack(anchor='e')
 
     #* Nastavení parametrů grafu - FRAME
         self.grafFrame = tk.LabelFrame(self, text='Parametry grafu')
@@ -63,7 +67,7 @@ class Application(tk.Tk):
         # Nastavení titulku grafu
         tk.Label(self.grafFrame, text='Titulek').grid(row=0, column=0)
         self.titleEntry = MyEntry(self.grafFrame)
-        self.titleENtry.grid(row=0, column=1)
+        self.titleEntry.grid(row=0, column=1)
 
         # Nastavení popisku osy x
         tk.Label(self.grafFrame, text='popisek X').grid(row=1, column=0)
@@ -84,6 +88,24 @@ class Application(tk.Tk):
         tk.Label(self.grafFrame, text='aproximace').grid(row=4, column=0)   
         self.selectAproximation = tk.Checkbutton(self.grafFrame)
         self.selectAproximation.grid(row=4, column=1, sticky='w')
+
+    #! Funkce na select souboru
+    def fileSelect(self, event=None):
+        self.filename = tk.filedialog.askopenfilename()
+        self.fileEntry.value = self.filename
+
+    def plotGraph(self):
+        with open(self.filename, 'r') as f:
+            if self.dataformatVar.get() == 1: #? data jsou ve sloupcích 
+                x = f.readline().split(';')
+                y = f.readline().split(';')
+                x = [float(i.replace(',', '.')) for i in x]
+                y = [float(i.replace(',', '.')) for i in y]
+
+            elif self.dataformatVar.get() == 1: #? data jsou v řádcích
+                pass
+        lab.plot(x,y,'o')
+        lab.show()
 
     #!Funkce na ukončení aplikace
     def quit(self, event=None):

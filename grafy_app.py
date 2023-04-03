ac24d94cd4b084bea49d03940e2370afb5e9de98
peoblouk <ahojcz48@gmail.com>
2023-04-03 10:37:50 +0200

    pylab problem!!

--- grafy_app.py
+++ grafy_app.py
@@ -3,7 +3,8 @@
 from os.path import basename, splitext
 import tkinter as tk
 from tkinter import filedialog # prohlížeč souborů
-import pylab as lab # grafy
+from tkinter import Menu
+import pylab as pl # grafy
 import scipy.interpolate as inp #https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.UnivariateSpline.html
 
 # from tkinter import ttk
@@ -40,6 +41,12 @@ class Application(tk.Tk):
         self.lbl = tk.Label(self, text="tkGraf")
         self.lbl.pack()
 
+    #* Main menu
+        self.mainMenu = tk.Menu(self)
+        self.mainMenu.add_command(label='O aplikaci')
+        self.mainMenu.add_separator()
+        self.mainMenu.add_command(label='Quit', command=self.quit, accelerator='ALT+F4')
+
     #* Výběr souboru - FRAME
         self.fileFrame = tk.LabelFrame(self, text='Soubor')
         self.fileFrame.pack(padx=5, pady=5, fill='x') # fill je roztažení ve směru osy x
@@ -62,17 +69,17 @@ class Application(tk.Tk):
         # Nastavení titulku grafu
         tk.Label(self.grafFrame, text='Titulek').grid(row=0, column=0)
         self.titleEntry = MyEntry(self.grafFrame)
-        self.titleEntry.grid(row=0, column=1)
+        self.titleEntry.grid(row=0, column=1, columnspan=2)
 
         # Nastavení popisku osy x
         tk.Label(self.grafFrame, text='popisek X').grid(row=1, column=0)
         self.xlabelEntry = MyEntry(self.grafFrame)
-        self.xlabelEntry.grid(row=1, column=1)
+        self.xlabelEntry.grid(row=1, column=1, columnspan=2)
 
         # Nastavení popisku osy y
         tk.Label(self.grafFrame, text='popisek Y').grid(row=2, column=0)
         self.ylabelEntry = MyEntry(self.grafFrame)
-        self.ylabelEntry.grid(row=2, column=1)
+        self.ylabelEntry.grid(row=2, column=1, columnspan=2)
 
         # Nastavení mřížky
         self.gridVar = tk.BooleanVar(value=False)
@@ -81,18 +88,34 @@ class Application(tk.Tk):
         self.gridChck.grid(row=3, column=1, sticky='w')
 
         # Nastavení stylu čáry
-        self.lineVar = tk.StringVar(value='none')
+        self.lineVar = tk.StringVar(value='None')
         tk.Label(self.grafFrame, text='styl čáry').grid(row=4, column=0)
-        self.lineOpt = tk.OptionMenu(self.grafFrame, self.lineVar, 'none', '-', ':', '--', '-.')
+        self.lineOpt = tk.OptionMenu(self.grafFrame, self.lineVar, 'None', '-', ':', '--', '-.')
         self.lineOpt.grid(row=4, column=1, sticky='w')
 
         # Nastaveni bodů
-        
+        self.markerVar = tk.StringVar(value='None')
+        tk.Label(self.grafFrame, text='styl marker').grid(row=5, column=0)
+        self.markerOpt = tk.OptionMenu(self.grafFrame, self.markerVar, 'None', *tuple(',.oxX+*P123<>v^D'))
+        self.markerOpt.grid(row=5, column=1, sticky='w')
+
+        # Nastavení barvy čáry
+        self.colorVar = tk.StringVar(value='black')
+        tk.Label(self.grafFrame, text='barva čáry').grid(row=6, column=0)
+        self.colorOpt = tk.OptionMenu(self.grafFrame, self.colorVar, "Black","Green", "Red", "Blue", "Purple", "Pink", "Yellow", "Grey", "Orange")
+        self.colorOpt.grid(row=6, column=1, sticky='w')
+
+        # Nastavení barvy markerů
+        self.mcolorVar =tk.StringVar(value='black')
+        tk.Label(self.grafFrame, text='barva marker').grid(row=7, column=0)
+        self.markerOpt = tk.OptionMenu(self.grafFrame, self.mcolorVar, "Black","Green", "Red", "Blue", "Purple", "Pink", "Yellow", "Grey", "Orange")
+        self.markerOpt.grid(row=7, column=1, sticky='w')
+
         # Nastavení aproximace
         self.aproxVar = tk.BooleanVar(value=False)
-        tk.Label(self.grafFrame, text='aproximace').grid(row=5, column=0)   
+        tk.Label(self.grafFrame, text='aproximace').grid(row=8, column=0)   
         self.selectAproximation = tk.Checkbutton(self.grafFrame, variable=self.aproxVar)
-        self.selectAproximation.grid(row=5, column=1, sticky='w')
+        self.selectAproximation.grid(row=8, column=1, sticky='w')
 
         #! Tlačítko kreslit
         tk.Button(self, text='Kreslit', command=self.plotGraph).pack(anchor='w')
@@ -126,7 +149,7 @@ class Application(tk.Tk):
                     x.append(x1)
                     y.append(y1)
             if self.aproxVar == True: # nastavení aproximace
-                newX = lab.linspace(0,3, 333)
+                newX = pl.linspace(0,3, 333)
                 # aproximace
                 # spl = inp.CubicSpline(x,y)
                 spl = inp.LSQUnivariateSpline(x,y) # metoda nejmenších čtverců
@@ -134,13 +157,14 @@ class Application(tk.Tk):
                 # spl = inp.BSpline(x,y, 3)
                 newY = spl(newX)
                 self
-    
-        lab.plot(x,y, 'o', linestyle=self.lineVar.get())
-        lab.title(self.titleEntry.value)
-        lab.xlabel(self.xlabelEntry.value)
-        lab.ylabel(self.ylabelEntry.value)
-        lab.grid(self.gridVar.get())
-        lab.show()
+            elif self.aproxVar == False: # nastavení aproximace
+                pass
+        pl.plot(x,y,'o',marker=self.markerVar.get(),linestyle=self.lineVar.get(),color=self.colorVar.get(), markeredgecolor=self.mcolorVar.get())
+        pl.title(self.titleEntry.value)
+        pl.xlabel(self.xlabelEntry.value)
+        pl.ylabel(self.ylabelEntry.value)
+        pl.grid(self.gridVar.get())
+        pl.show()
 
     #!Funkce na ukončení aplikace
     def quit(self, event=None):
